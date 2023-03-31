5d75d212666da204f4169e48473a0d00907a142f
Jack theDev <jakub.wiktor.adamczyk@gmail.com>
2023-03-31 19:40:46 +0200

    user-avatar-content

--- src/components/feature/Navigation.jsx
+++ src/components/feature/Navigation.jsx
@@ -4,13 +4,22 @@ import { FaBeer } from "react-icons/fa";
 import { AiFillHome } from "react-icons/ai";
 import { MdVideoLibrary } from "react-icons/md";
 import { IoAdd } from "react-icons/io5";
+import reactLogo from './../../assets/user.png';
+
 
 import "./Navigation.css";
 function Naviagation ()
 {
   const iconStyle={ color: "white", fontSize: "1.5em"}
+ 
     return(
-        <ul className="naviagtion">
+      <section className="naviagtion">
+        
+        <div className="user-navigation">
+        <img id="user-profile-img" src={reactLogo}/>
+        <p>Hi kuba</p>
+        </div>
+        <ul>
           <li >
             <div className="naviagation-content">
             <AiFillHome style={iconStyle}/>
@@ -36,7 +45,7 @@ function Naviagation ()
             </div>
           </li>
         </ul>
-        
+        </section>
     )
 }
   
