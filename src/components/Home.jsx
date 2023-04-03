50e8e3479988cefc2ccb371409efcb3be7a53185
Jack theDev <jakub.wiktor.adamczyk@gmail.com>
2023-04-03 16:19:12 +0200

    localJsonTutorialTest

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import './Home.css'
 import { AiOutlineSearch } from "react-icons/ai";
+import tutorialData from '../data.json'
 
 function Home ()
 {
 
+    console.log(tutorialData.video[0])
+    const videoTest=tutorialData.video[0];
  return(
         <section className='home-section'>
             <div className='home-content'>
@@ -14,8 +17,8 @@ function Home ()
                 </div>
                 <div className='grid-with-home-content'>
                     <div>
-                        <img/>
-                        <h1>text-for begginers</h1>
+                        <img src={videoTest.Image}/>
+                        <h1>{videoTest.tutorialName}</h1>
                         <p>canal-name</p>
                         <p>hour-likes</p>
                     </div> 
