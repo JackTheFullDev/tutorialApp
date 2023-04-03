2407ec910174c63432b550e629fb52776470bd86
Jack theDev <jakub.wiktor.adamczyk@gmail.com>
2023-04-03 23:21:29 +0200

    preview-tutorial-card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,23 +16,24 @@ function Home() {
           <AiOutlineSearch id="search-icon" />
         </div>
         <div className="grid-with-home-content">
-          {tutorialData.video.map((data) => {
-            {console.log(data.tutorialName)}
-            <div className="tutorial-component">
-              <img src={pythonImg} />
-              <div className="inner-info-content">
+        {tutorialData.video.map((data) => {
+    return (
+        <div className="tutorial-component">
+            <img src={pythonImg} />
+            <div className="inner-info-content">
                 <h1 id="tutorial-name">{data.tutorialName}</h1>
                 <p id="canal-name">{data.canalName}</p>
                 <div className="bottom-tutorial-components">
-                  <div className="bottom-totalhour-likes">
-                    <p>{data.totalHour}h video</p>
-                    <p>{data.totalHour}Likes</p>
-                  </div>
-                  <button>Register</button>
+                    <div className="bottom-totalhour-likes">
+                        <p>{data.totalHour}h video</p>
+                        <p>{data.totalHour}Likes</p>
+                    </div>
+                    <button>Register</button>
                 </div>
-              </div>
-            </div>;
-          })}
+            </div>
+        </div>
+    );
+})}
         </div>
       </div>
     </section>
