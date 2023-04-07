bb1c6d40709f077151ee9f42f388b7c47ad2b449
Jack theDev <jakub.wiktor.adamczyk@gmail.com>
2023-04-07 13:48:05 +0200

    searchbar-byTutorialName-byCanalName

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,48 +5,59 @@ import pythonImg from "/assets/python-tutorial.jpg";
 import { useState } from "react";
 
 function Home() {
- 
   const [inputValue, setInputValue] = useState("");
-  const handleHomeInput = (event) =>
-  {
-    const newValue=event.target.value.toLowerCase();
+  const handleHomeInput = (event) => {
+    const newValue = event.target.value.toLowerCase();
     setInputValue(newValue);
-  }
+  };
   return (
     <section className="home-section">
       <div className="home-content">
         <div className="searchBar">
-          <input className="searchbar-input" placeholder="search"  onChange={handleHomeInput} />
+          <input
+            className="searchbar-input"
+            placeholder="search"
+            onChange={handleHomeInput}
+          />
           <AiOutlineSearch id="search-icon" />
         </div>
         <div className="grid-with-home-content">
-
-          {console.log(tutorialData.video.filter(searching=>searching.tutorialName.toLowerCase().includes(inputValue)))}
-        {tutorialData.video.map((data,index) => {
-    return (
-        <div className="tutorial-component" key={index}> 
-             <img src={data.Image} alt={data.tutorialName} />
-             <div className="tutorial-complex-data">
-               <p>Start date: {data.startDate}</p>
-               <p>End date: {data.endDate}</p>
-               <p>day/s: {data.days.join("-")} </p>
-               <p>amount: {data.amount}/{data["max-amount"]}</p>
-               <p>level: {data.level}</p>
-               </div>
-            <div className="inner-info-content">
-                <h1 id="tutorial-name">{data.tutorialName}</h1>
-                <p id="canal-name">{data.canalName}</p>
-                <div className="bottom-tutorial-components">
-                    <div className="bottom-totalhour-likes">
+          {tutorialData.video
+            .filter((searchingValue) => {
+              return (
+                searchingValue.tutorialName
+                  .toLocaleLowerCase()
+                  .includes(inputValue) ||
+                searchingValue.canalName.toLowerCase().includes(inputValue)
+              );
+            })
+            .map((data, index) => {
+              return (
+                <div className="tutorial-component" key={index}>
+                  <img src={data.Image} alt={data.tutorialName} />
+                  <div className="tutorial-complex-data">
+                    <p>Start date: {data.startDate}</p>
+                    <p>End date: {data.endDate}</p>
+                    <p>day/s: {data.days.join("-")} </p>
+                    <p>
+                      amount: {data.amount}/{data["max-amount"]}
+                    </p>
+                    <p>level: {data.level}</p>
+                  </div>
+                  <div className="inner-info-content">
+                    <h1 id="tutorial-name">{data.tutorialName}</h1>
+                    <p id="canal-name">{data.canalName}</p>
+                    <div className="bottom-tutorial-components">
+                      <div className="bottom-totalhour-likes">
                         <p>{data.totalHour}h video</p>
                         <p>{data.totalHour}Likes</p>
+                      </div>
+                      <button>Register</button>
                     </div>
-                    <button>Register</button>
+                  </div>
                 </div>
-            </div>
-        </div>
-    );
-})}
+              );
+            })}
         </div>
       </div>
     </section>
