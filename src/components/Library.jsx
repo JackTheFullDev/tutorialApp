d2fef52264a505f1694ca80e036f8c34694b4813
Jack theDev <jakub.wiktor.adamczyk@gmail.com>
2023-04-16 00:26:46 +0200

    fixed error

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -29,30 +29,7 @@ export const Library = () => {
 
           {addTutorialData.map((ownContentData, index) => {
                 return (
-                  <div className="tutorial-component" key={index}>
-                    <img src={ownContentData._addTutorialData.selectedImage}/>
-                    <div className="tutorial-complex-data">
-                      <p>Start date: {ownContentData._addTutorialData.tutorialStartDate}</p>
-                      <p>End date: {ownContentData._addTutorialData.tutorialEndDate}</p>
-                      <p>day/s: {ownContentData._addTutorialData.tutorialDays.join("-")} </p>
-                      <p>
-                        amount: 0/{ownContentData._addTutorialData.tutorialNumberOfPeople}
-                      </p>
-                      <p>level: {ownContentData._addTutorialData.tutorialLevel}</p>
-                    </div>
-                    <div className="inner-info-content">
-                      <h1 id="tutorial-name">{ownContentData._addTutorialData.tutorialName}</h1>
-                      <p id="canal-name">MyCanalName</p>
-                      <div className="bottom-tutorial-components">
-                        <div className="bottom-totalhour-likes">
-                          <p>h video</p>
-                          <p>Likes</p>
-                        </div>
-                        <button style={{color:"yellow"}} onClick={()=>(removeFromLibrary(data))}>Resign</button>
-                      </div>
-                    </div>
-                  </div>
-                 
+                  
                 );
               })}
 
